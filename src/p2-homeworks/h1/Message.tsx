e3e156d4d7412c4cefc6eed204e4355e89d59a53
Matrix <o.korsakov@matrixb2b.net>
2021-06-04 14:18:17 +0300

    end HW2

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import s from './Message.module.css'
 
-type MessageType = {
+type MessagePropsType = {
     avatar: string
     name: string
     message: string
     time: string
 }
 
-function Message(props: MessageType) {
+function Message(props: MessagePropsType) {
     return (
         <div className={s.wrapper}>
             <img className={s.ava} src={props.avatar} alt="avatar"/>
+            <div className={s.triangle}></div>
             <div className={s.message}>
                 <div className={s.bodyOfMessage}>
                     <span className={s.name}>{props.name}</span>
                     <span className={s.text}>{props.message}</span>
                 </div>
-                <div className={s.timeOfMessage}><span>{props.time}</span></div>
+                <div className={s.timeOfMessage}>{props.time}</div>
             </div>
         </div>
     )
--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
+import {AffairType} from './HW2'
 
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair: AffairType // need to fix any
+    deleteAffairCallback:  any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+    const deleteCallback = (id: number) => {props.deleteAffairCallback(id)} // need to fix
 
     return (
         <div>
             // show some text
-
-            <button onClick={deleteCallback}>X</button>
+            <button onClick={() => deleteCallback(props.affair._id)}>X</button>
         </div>
     )
 }
