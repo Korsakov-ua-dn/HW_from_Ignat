06878fe6db8bb222752ee18d5b9fe04bf11be2a0
korsakov <korsakov.ua.dn@gmail.com>
2021-07-07 21:16:30 +0300

    end HW5 without css

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import s from './HW5.module.css'
+import {PATH} from './Routes'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <div><NavLink to={PATH.PRE_JUNIOR} activeClassName={s.active}>pre-junior</NavLink></div>
+            <div><NavLink to={PATH.JUNIOR} activeClassName={s.active}>junior</NavLink></div>
+            <div><NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>junior+</NavLink></div>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import HW12 from '../../h12/HW12'
+// import HW13 from '../../h13/HW13'
+// import HW9 from '../../h9/HW9'
+// import HW10 from '../../h10/HW10'
+// import HW11 from '../../h11/HW11'
+
+function JuniorPlus() {
+    return (
+        <div>
+            {/*<HW12/>*/}
+            {/*<HW13/>*/}
+            {/*<HW9/>*/}
+            {/*<HW10/>*/}
+            {/*<HW11/>*/}
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
