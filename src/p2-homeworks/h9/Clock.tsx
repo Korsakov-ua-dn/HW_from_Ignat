0078922dc09a0d653530893769f79625eea3f820
Matrix <o.korsakov@matrixb2b.net>
2021-09-07 10:55:49 +0300

    end HW9 without css

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,31 +1,32 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import style from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
-            // setDate
+        const id: number = window.setInterval(() => { // необходимо явно привести в числу, можно поставить + перед setinterval
+            setDate( new Date() )
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleTimeString() // fix with date
+    const stringDate = date.toLocaleDateString() // fix with date
 
     return (
         <div>
@@ -36,11 +37,9 @@ function Clock() {
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
-                    {stringDate}
-                </div>
-            )}
+            <div className={style.date}>
+                {show && stringDate}
+            </div>
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
