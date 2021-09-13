9b2a631e7e3ce128a14dd472998ff98969b8a57e
Matrix <o.korsakov@matrixb2b.net>
2021-09-13 09:06:14 +0300

    Updates

---
+++ precache-manifest.f0b0f2a9d9597657c089a77105e97c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8ad2b4babca5b587ab3def4b2d4a08",
+    "url": "/HW_from_Ignat/index.html"
+  },
+  {
+    "revision": "021eddc90a692a9298a1",
+    "url": "/HW_from_Ignat/static/css/main.5f0bcdf2.chunk.css"
+  },
+  {
+    "revision": "a82c00f532e05ba81326",
+    "url": "/HW_from_Ignat/static/js/2.88eb164e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/HW_from_Ignat/static/js/2.88eb164e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "021eddc90a692a9298a1",
+    "url": "/HW_from_Ignat/static/js/main.63ae6219.chunk.js"
+  },
+  {
+    "revision": "789970aa1ca820eb6a0f",
+    "url": "/HW_from_Ignat/static/js/runtime-main.7b5b7576.js"
+  }
+]);
